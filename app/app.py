d602525f9642ba800d242ee2dcc8efb3911701e2
Pankaj <pnkcin@gmail.com>
2026-03-11 22:22:14 +0530

    public-ip

--- app/app.py
+++ app/app.py
@@ -25,3 +25,4 @@ return 'CI/CD LIVE! Deployed at Wed Mar 11 21:47:26 IST 2026'
 # Auto-deploy test
 return f'WEBHOOK TEST 'Wed Mar 11 21:57:17 IST 2026
 return f'PUBLIC WEBHOOK {{time.ctime()}}! Logs: {{result}}'
+return f'PUBLIC 38.254.189.62 LIVE {time.ctime()}! {{result}}'
